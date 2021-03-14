aa3a8951cb2532bf8b2b95ef2984b6ef58f73957
Andrea Zoppi <texzk@email.it>
2021-03-14 17:22:52 +0100

    Added reference to Cython source

--- scripts/cython_build_tests.py
+++ scripts/cython_build_tests.py
@@ -1,7 +1,6 @@
 import os
-import sys
 
 # working directory = repository root folder
-sys.path.append('src')
+os.environ['PYTHONPATH'] = os.path.abspath('src')
 path = os.path.join('tests', '_test_c.pyx')
 os.system(r'cythonize -f -a -i ' + path)
