908fa786626d976a4ace97f7fa054d3fe01210c3
TexZK <texzk@email.it>
2021-03-31 22:19:40 +0200

    More docs

--- src/bytesparse/_py.py
+++ src/bytesparse/_py.py
@@ -23,6 +23,8 @@
 # ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 # POSSIBILITY OF SUCH DAMAGE.
 
+r"""Python implementation."""
+
 from itertools import count as _count
 from itertools import islice as _islice
 from itertools import repeat as _repeat
